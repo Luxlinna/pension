eabbf008f52079f92deac44b5aee525dab711a17
luxlinna <luxlinna@gmail.com>
2021-04-15 22:16:07 +0200

    update all files

--- Program.cs
+++ Program.cs
@@ -1,33 +1,61 @@
 ﻿using System;
 
-namespace Pension1
+namespace Pension
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string förnamn;
-            string efternamn;
-            int ålder;
+            int pensionÅlder = 65;
             int pensionen;
 
             Console.BackgroundColor = ConsoleColor.Blue;
             
             Console.Write("Skriv in ditt förnamn: ");
-            förnamn = (Console.ReadLine());
+            string förnamn = Console.ReadLine();
+            if (förnamn != "")
+            {
+                Console.Write("Skriv in ditt efternamn: ");
+                string efternamn = Console.ReadLine();
+                if (efternamn != "")
+                {
+                    Console.Write("Skriv in ditt ålder: ");
+                    int ålder = Convert.ToInt32(Console.ReadLine());
+                    if (ålder > 0)
+                    {
+                        pensionen = (pensionÅlder - ålder);
 
-            Console.Write("Skriv in ditt efternamn: ");
-            efternamn = Console.ReadLine();
+                        if (pensionen > 0)
+                        {
+                            Console.WriteLine("Hej " + förnamn.ToUpper() + " " + efternamn.ToUpper() + "! " + "Det är nu " + pensionen + " år kvar till du går i pension." );
+                        } 
+                        else if (pensionen == 0)
+                        {
+                            Console.WriteLine("Hej " + förnamn.ToUpper() + " " + efternamn.ToUpper() + "! " + "Du blir pensionär i år." );
+                        } 
+                        else
+                        {
+                            Console.WriteLine("Hej " + förnamn.ToUpper() + " " + efternamn.ToUpper() + "! " + "Du är redan pensionär." );
+                        }
 
-            Console.Write("Skriv in ditt ålder: ");
-            ålder = Convert.ToInt32(Console.ReadLine());
-
-            pensionen = (65 - ålder);
-
-
-            Console.WriteLine("Hej " + förnamn.ToUpper() + " " + efternamn.ToUpper() + "! " + "Det är nu " + pensionen + " år kvar till du går i pension." );
-
-            Console.ReadKey();
+                        Console.ReadKey();
+                    } 
+                    else
+                    {
+                        Console.WriteLine("Det är inte din ålder !!!");
+                        Console.ReadKey();
+                    }
+                } 
+                else
+                {
+                    Console.Write("Du glömmer att skriva ditt Efternamn!!!");
+                    Console.ReadKey();
+                }
+            } else
+            {
+                Console.Write("Du glömmer att skriva ditt Förnamn!!!");
+                Console.ReadKey();
+            }       
         }
     }
 }
